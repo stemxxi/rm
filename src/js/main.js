b41ebad1a4fd98e61d0b909692517c0b01409d41
timur <timurkakhkharov@gmail.com>
2021-12-19 18:40:20 +0500

    mh

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('.navbar-toggler-icon');
+const navActive = document.querySelector('.navbar-new-toggler');
+
+
+
+button.addEventListener('click', () => {
+    button.classList.add('active-none');
+    navActive.classList.remove('active-none')
+})
+navActive.addEventListener('click', () => {
+    button.classList.remove('active-none');
+    navActive.classList.add('active-none')
+})
