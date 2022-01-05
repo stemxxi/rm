d8aa9eebf377f2c6316cea41c909bc70bb74e586
timur <timurkakhkharov@gmail.com>
2022-01-06 00:00:12 +0500

    sd

--- src/js/main.js
+++ src/js/main.js
@@ -101,109 +101,109 @@ navActive.addEventListener('click', () => {
 
 
 
-$(document).ready(function(){
-  $('.center-slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    centerMode: true,
-    arrows: true,
-    dots: false,
-    speed: 300,
-    centerPadding: '30px',
-    infinite: true,
-    autoplaySpeed: 4000,
-    // autoplay: true,
-    responsive: [
-          {
-            breakpoint: 1201,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '50px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 993,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '30px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 769,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '60px',
-              slidesToShow: 2
-            }
-          },
-          {
-            breakpoint: 577,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '50px',
-              slidesToShow: 2
-            }
-          },
-          {
-            breakpoint: 531,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '50px',
-              slidesToShow: 1
-            }
-          },
-          {
-            breakpoint: 520,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '50px',
-              slidesToShow: 1
-            }
-          },
-          {
-            breakpoint: 476,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '50px',
-              slidesToShow: 1
-            }
-          },
-          {
-            breakpoint: 400,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '50px',
-              slidesToShow: 1
-            }
-          },
-          {
-            breakpoint: 376,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          },
-          {
-            breakpoint: 340,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '30px',
-              slidesToShow: 1
-            }
-          },
-        ]
-  });
-});
+// $(document).ready(function(){
+//   $('.center-slider').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     centerMode: true,
+//     arrows: true,
+//     dots: false,
+//     speed: 300,
+//     centerPadding: '30px',
+//     infinite: true,
+//     autoplaySpeed: 4000,
+//     // autoplay: true,
+//     responsive: [
+//           {
+//             breakpoint: 1201,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '50px',
+//               slidesToShow: 3
+//             }
+//           },
+//           {
+//             breakpoint: 993,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '30px',
+//               slidesToShow: 3
+//             }
+//           },
+//           {
+//             breakpoint: 769,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '60px',
+//               slidesToShow: 2
+//             }
+//           },
+//           {
+//             breakpoint: 577,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '50px',
+//               slidesToShow: 2
+//             }
+//           },
+//           {
+//             breakpoint: 531,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '50px',
+//               slidesToShow: 1
+//             }
+//           },
+//           {
+//             breakpoint: 520,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '50px',
+//               slidesToShow: 1
+//             }
+//           },
+//           {
+//             breakpoint: 476,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '50px',
+//               slidesToShow: 1
+//             }
+//           },
+//           {
+//             breakpoint: 400,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '50px',
+//               slidesToShow: 1
+//             }
+//           },
+//           {
+//             breakpoint: 376,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '40px',
+//               slidesToShow: 1
+//             }
+//           },
+//           {
+//             breakpoint: 340,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '30px',
+//               slidesToShow: 1
+//             }
+//           },
+//         ]
+//   });
+// });
